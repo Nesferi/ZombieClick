3c1d6770403aca2404819098d048a3f56ea9a11b
nesferi <nesferi@gmail.com>
2018-01-10 19:30:50 +0100

    Iniciamos el proyecto

---
+++ app/src/main/java/com/example/nestorfernandez/zombieclick/GameView.java
@@ -0,0 +1,26 @@
+package com.example.nestorfernandez.zombieclick;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.view.View;
+
+/**
+ * Created by nestor.fernandez on 10/01/2018.
+ */
+
+public class GameView extends View {
+    private Bitmap bmp;
+    public GameView(Context context) {
+        super(context);
+        bmp = BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawColor(Color.BLACK);
+        canvas.drawBitmap(bmp,10,10,null);
+    }
+}
